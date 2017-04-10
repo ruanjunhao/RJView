db4e9e95c9573d22e351ab58f1f2ac7b2ed678ce
ruan037 <ruan037@126.com>
2017-04-10 21:06:32 +0800

    xx

---
+++ Example/Pods/Target Support Files/Pods-RJView_Tests/Pods-RJView_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RJView_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_RJView_Tests
+@end
---
+++ Example/Pods/Target Support Files/RJView/RJView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_RJView : NSObject
+@end
+@implementation PodsDummy_RJView
+@end
